ffc1d5dc2379804694796aa17b4ff4cff7be44ce
Royal <royal.21ai040@sode-edu.in>
2025-01-10 23:37:10 +0530

    add trend context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-
-
-
-import './App.css'
+import Navbar from "./components/Navbar"
+import CoinPage from "./pages/CoinPage"
 
 function App() {
-
-
   return (
-    <h1 className="text-3xl font-bold underline text-center text-blue-500">
-    Hello world!
-  </h1>
-
+    <div>
+       <Navbar/>
+       <CoinPage/>
+    </div>
   )
 }
 
---
+++ src/context/trend.tsx
@@ -0,0 +1,18 @@
+//@ts-nocheck
+import { useState, useContext, createContext } from "react";
+
+const TrendContext = createContext();
+const TrendProvider = ({ children }) => {
+  const [trend, setTrend] = useState([]);
+
+  return (
+    <TrendContext.Provider value={[trend, setTrend]}>
+      {children}
+    </TrendContext.Provider>
+  );
+};
+
+// custom hook
+const useTrend = () => useContext(TrendContext);
+
+export { useTrend, TrendProvider };
